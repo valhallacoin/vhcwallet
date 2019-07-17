459179017e28a34a48be0688fa8bb742ec839752
Idunna <idunna@cryptogroup.net>
2019-07-17 14:40:01 +0000

    activate DCP0001-DCP0003 on mainnet's genesis

--- deployments/deployments.go
+++ deployments/deployments.go
@@ -25,7 +25,7 @@ type HardcodedDeployment struct {
 // DCP0001 specifies hard forking changes to the stake difficulty algorithm as
 // defined by https://github.com/valhallacoin/dcps/blob/master/dcp-0001/dcp-0001.mediawiki.
 var DCP0001 = HardcodedDeployment{
-	MainNetActivationHeight: 149248,
+	MainNetActivationHeight: 0,
 	TestNetActivationHeight: 0,
 	SimNetActivationHeight:  0,
 }
@@ -33,7 +33,7 @@ var DCP0001 = HardcodedDeployment{
 // DCP0002 specifies the activation of the OP_SHA256 hard fork as defined by
 // https://github.com/valhallacoin/dcps/blob/master/dcp-0002/dcp-0002.mediawiki.
 var DCP0002 = HardcodedDeployment{
-	MainNetActivationHeight: 189568,
+	MainNetActivationHeight: 0,
 	TestNetActivationHeight: 0,
 	SimNetActivationHeight:  0,
 }
@@ -41,7 +41,7 @@ var DCP0002 = HardcodedDeployment{
 // DCP0003 specifies the activation of a CSV soft fork as defined by
 // https://github.com/valhallacoin/dcps/blob/master/dcp-0003/dcp-0003.mediawiki.
 var DCP0003 = HardcodedDeployment{
-	MainNetActivationHeight: 189568,
+	MainNetActivationHeight: 0,
 	TestNetActivationHeight: 0,
 	SimNetActivationHeight:  0,
 }
